6127ad4e14422769f267f00d5ef5ea34373cae0a
naomi curapil <curapilnaomi@icloud.com>
2021-11-08 19:08:38 -0600

    commit tree 815c6fc2390e96708040fb4b7987c50b7e896844

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+const newText = document.querySelector('p');
+   document.addEventListener('DOMContentLoaded', () => { 
+    updateDOM()
+    console.log('DOM fully loaded and parsed');
+})
+
+function updateDOM(){ 
+    document.getElementById('text').textContent = 'This is really cool!'
+
+}
+   
+   
+    
